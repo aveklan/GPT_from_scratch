55a21adab4b858df9b3c7c7591f20e30be7dea92
aveklan <avesani55@gmail.com>
2026-01-21 09:54:08 -0500

    Added optimized version of Adam

--- train_gpt2.py
+++ train_gpt2.py
@@ -183,7 +183,30 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
-
+    
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+       
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
 
 num_return_sequences = 5
 max_len = 30
@@ -239,8 +262,28 @@ model = GPT(GPTConfig(vocab_size=50304))
 model = model.to(device)
 model = torch.compile(model)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-for i in range(328):
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+
+# optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, betas=(0.9, 0.95), eps=1e-8)
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=max_lr, device_type=device)
+
+for step in range(max_steps):
     t0 = time.time()
     x,y = train_loaDer.next_batch()
     x,y = x.to(device), y.to(device)
@@ -248,12 +291,19 @@ for i in range(328):
     with torch.autocast(device_type=device, dtype=torch.bfloat16):                  
         logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+    # determine and set the learning rate for this iteration
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
     optimizer.step()
-    torch.cuda.synchronize() if device == 'cuda' else None
+    torch.cuda.synchronize() if device == 'cuda' else None #Wait for the GPU to finish
     t1 = time.time()
     dt = (t1 - t0) * 1000
     tokens_per_sec = (train_loaDer.B * train_loaDer.T) / (t1 - t0)
-    print(f"step {i+1}, loss: {loss.item():.4f}, time: {dt:.2f} ms, tokens/sec: {tokens_per_sec:.2f}")
+    print(f"step {step+1}, loss: {loss.item():.4f}, lr: {lr:.4e}, norm: {norm:.4f} ,time: {dt:.2f} ms, tokens/sec: {tokens_per_sec:.2f}")
     
 
 import sys; sys.exit(0)
